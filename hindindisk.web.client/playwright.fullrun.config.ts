e07c40a549e41522abd1617d02be47d11a313ed9
Developer <Developer@KL-ITS-DEVELOP2>
2026-07-11 17:42:45 +0530

    Playwrite config

---
+++ hindindisk.web.client/playwright.fullrun.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: false,
+  workers: 1,
+  forbidOnly: false,
+  retries: 0,
+  reporter: 'line',
+  timeout: 60000,
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'off',
+    ignoreHTTPSErrors: true,
+    launchOptions: {
+      slowMo: 1500,
+      headless: false,
+      args: ["--window-size=1280,900"],
+    },
+  },
+  projects: [
+    { name: 'chromium', use: { browserName: 'chromium' } },
+  ],
+});
